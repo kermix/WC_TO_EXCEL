e9d74c09209f4463ebee6ec8150fc783ec3abfb9
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-03 14:03:52 +0200

    added time to logs

--- app/misc.py
+++ app/misc.py
@@ -1,3 +1,21 @@
+import logging
+import sys
+
+def __setup_logger(name):
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.FileHandler('log.txt', mode='w')
+    handler.setFormatter(formatter)
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    logger.addHandler(screen_handler)
+    return logger
+
+logger = __setup_logger(__name__)
+
 def safe_cast(val, to_type, default=None):
     try:
         return to_type(val)
