91e350ea13c2ae75fb0421033e3285733b29e5bf
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-06 10:01:14 +0200

    Added setter for fields property in Product class

--- app/product.py
+++ app/product.py
@@ -6,11 +6,24 @@ class Product:
     def __init__(self):
         self.fields = []
 
+    @property
+    def fields(self):
+        return self.__fields
+
+    @fields.setter
+    def fields(self, fields):
+        for field in fields:
+            if not isinstance(field, (Field, MetaField)):
+                raise TypeError(f"field is '{type(field)}' object. Should be one of these types: Field, MetaField")
+
+            self.fields.append(field)
+
     def add_field(self, field):
         if not isinstance(field, (Field, MetaField)):
-            raise TypeError(f"field is '{type(field)}'should be one of these types: Field, MetaField")
+            raise TypeError(f"field is '{type(field)}' object. Should be one of these types: Field, MetaField")
 
         self.fields.append(field)
 
     def to_serie(self):
         pass
+
