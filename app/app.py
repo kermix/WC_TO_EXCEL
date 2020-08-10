0465c233ec3b487ce695751cc655c017dee08a1a
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-10 09:28:54 +0200

    Added support to field sets

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
 from api import get_variants_as_products
 
-d = get_variants_as_products()
+from field_sets import get_required_field_set
+
+d = get_variants_as_products(fieldset=get_required_field_set())
---
+++ app/field_sets.py
@@ -0,0 +1,23 @@
+from field import Field, MetaField, VariantAttributeField
+
+from field_filters import *
+
+
+# Project is optimised for BioCompare DB format
+
+def get_header_from_field_set(self):
+    importance_levels = []
+    field_header = []
+    for field in self.fields:
+        importance_levels.append(field.importance_level)
+        field_header.append(field.field_name)
+    return importance_levels, field_header
+
+
+def get_required_field_set():
+    return [Field(key_name='name', importance_level="Required", field_name='Name'),
+    Field(key_name='sku', importance_level="Required", field_name='Catalog Number'),
+    Field(key_name='permalink', importance_level="Required", field_name='URL'),
+    VariantAttributeField(variant_names=('Classes', 'Quantity', 'Exctractions', 'Unit', 'Reactions',
+                                         'Weight', 'Volume'),
+                          optional_meta_name='quantity', importance_level="Required", field_name='Quantity')]
