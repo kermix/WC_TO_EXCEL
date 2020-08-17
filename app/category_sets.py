7ade2abdfa14a5b1c658f44411fa81eb1021312f
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-17 17:52:57 +0200

    Added CategorySet class

---
+++ app/category_sets.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from api import get_variants_as_products
+
+from misc import to_comma_separated_string
+
+class CategorySet:
+    def __init__(self, categories, exclude, fieldset):
+        self.categories = categories
+        self.exclude = exclude
+        self.fieldset = fieldset
+        self.dataset = None
+
+    def populate_data(self):
+        categories = to_comma_separated_string(self.categories)
+        exclude = to_comma_separated_string(self.exclude)
+        self.dataset = get_variants_as_products(
+                            fieldset=self.fieldset,
+                            product_get_params={
+                                "category": categories,
+                                "exclude": exclude,
+                            })
+
+    def to_dataframe(self):
+        df = pd.DataFrame()
+
+        for product in self.dataset:
+            series = product.to_series()
+            df = df.append(series, ignore_index=True)
+        return df
+
