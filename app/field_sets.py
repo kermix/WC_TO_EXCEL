34b118c9f801ba2200fe3d6deaf96e6b6c8534e1
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-17 17:53:53 +0200

    Extended example field_sets.py file

--- app/field_sets.py
+++ app/field_sets.py
@@ -19,5 +19,16 @@ def get_required_field_set():
     VariantAttributeField(variant_names=('sku',), optional_meta_name='sku', importance_level="Required", field_name='Catalog Number'),
     Field(key_name='permalink', importance_level="Required", field_name='URL'),
     VariantAttributeField(variant_names=('Classes', 'Quantity', 'Exctractions', 'Unit', 'Reactions',
-                                         'Weight', 'Volume'),
+                                         'Weight', 'Volume', 'Size'),
                           optional_meta_name='quantity', importance_level="Required", field_name='Quantity')]
+
+def get_enzymes_field_set():
+    return get_required_field_set() + [
+        MetaField(key_name="enzyme_family", importance_level="Recommended", field_name="Enzyme Family (Enzymes only)"),
+        MetaField(key_name="enzyme_name", importance_level="Recommended", field_name="Enzyme Subfamily (Enzymes only)"),
+        MetaField(key_name="enzyme_features", importance_level="Recommended",
+                  field_name="Enzyme Features (Enzymes only)"),
+        MetaField(key_name="restriction_enzyme", importance_level="Recommended",
+                  field_name="Restriction Enzyme (Enzymes only)"),
+    ]
+
