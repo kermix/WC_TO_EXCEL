91dd102d76c7ec47e8e0e75f3c595a739effe731
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-17 17:56:31 +0200

    app.py adjusted to usage of CategorySet

--- app/app.py
+++ app/app.py
@@ -1,5 +1,12 @@
-from api import get_variants_as_products
+import pandas as pd
 
-from field_sets import get_required_field_set
+from field_sets import get_enzymes_field_set
 
-d = get_variants_as_products(fieldset=get_required_field_set())
+from category_sets import CategorySet
+
+# Project is optimised for BioCompare DB format
+
+cs = CategorySet(categories=48, exclude=[1004, 991], fieldset=get_enzymes_field_set())
+cs.populate_data()
+
+df = cs.to_dataframe()
