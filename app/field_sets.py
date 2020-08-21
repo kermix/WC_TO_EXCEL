d5793280853308a1581df8ba01281662ae83d250
Mateusz Jamrowski <kermaxpl@gmail.com>
2020-08-21 10:28:21 +0200

    Fixed creating dataframe with proper multiindex columns header

--- app/field_sets.py
+++ app/field_sets.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from field import Field, MetaField, VariantAttributeField
 
 from field_filters import *
@@ -11,7 +13,7 @@ def get_header_from_field_set(fields):
     for field in fields:
         importance_levels.append(field.importance_level)
         field_header.append(field.field_name)
-    return importance_levels, field_header
+    return pd.MultiIndex.from_arrays((importance_levels, field_header))
 
 
 def get_required_field_set():
@@ -22,8 +24,10 @@ def get_required_field_set():
                                          'Weight', 'Volume', 'Size'),
                           optional_meta_name='quantity', importance_level="Required", field_name='Quantity')]
 
+
 def get_enzymes_field_set():
     return get_required_field_set() + [
+        Field(key_name='price', importance_level='Recommended', field_name='Euro price', variant_field=True),
         MetaField(key_name="enzyme_family", importance_level="Recommended", field_name="Enzyme Family (Enzymes only)"),
         MetaField(key_name="enzyme_name", importance_level="Recommended", field_name="Enzyme Subfamily (Enzymes only)"),
         MetaField(key_name="enzyme_features", importance_level="Recommended",
@@ -32,3 +36,27 @@ def get_enzymes_field_set():
                   field_name="Restriction Enzyme (Enzymes only)"),
     ]
 
+
+def get_extraction_kits_field_set():
+    return get_required_field_set() + [
+        Field(key_name='price', importance_level='Recommended', field_name='Euro price', variant_field=True),
+        Field(key_name='description', importance_level='Optional', field_name='Description'),
+        Field(key_name='category', importance_level='Optional', field_name='category'),
+        Field(key_name='references', importance_level='Optional', field_name='Product-specific References'),
+        MetaField(key_name='isolated_material', importance_level='Custom', field_name='Isolated Material'),
+        MetaField(key_name='kit_components', importance_level='Custom', field_name='Extraction kits'),
+        MetaField(key_name='purification_step', importance_level='Custom', field_name='Sample Material'),
+        MetaField(key_name='efficiency', importance_level='Custom', field_name='Efficiency'),
+        MetaField(key_name='binding_capacity', importance_level='Custom', field_name='Binding Capacity'),
+        MetaField(key_name='time_required', importance_level='Custom', field_name='Time Required'),
+        MetaField(key_name='yield_purity', importance_level='Custom', field_name='Yield Purity')
+    ]
+
+def get_extraction_columns_field_set():
+    return get_required_field_set() + [
+        Field(key_name='price', importance_level='Recommended', field_name='Euro price', variant_field=True),
+        Field(key_name='description', importance_level='Optional', field_name='Description'),
+        Field(key_name='category', importance_level='Optional', field_name='category'),
+        Field(key_name='references', importance_level='Optional', field_name='Product-specific References'),
+        MetaField(key_name='type', importance_level='Custom', field_name='Type ')
+    ]
